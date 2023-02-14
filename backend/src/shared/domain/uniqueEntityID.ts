61acbad433ba5de9e2d70f10e8ff15fe7b97fca2
shun57 <tuktuk.032@gmail.com>
2023-02-15 00:32:02 +0900

    fix: ファイル名をキャメルケースに修正

--- backend/src/shared/domain/uniqueEntityID.ts
+++ backend/src/shared/domain/uniqueEntityID.ts
@@ -1,5 +1,5 @@
 import { uuid } from 'uuidv4'
-import { Identifier } from './Identifier'
+import { Identifier } from './identifier'
 
 export class UniqueEntityID extends Identifier<string | number> {
   constructor(id?: string | number) {
