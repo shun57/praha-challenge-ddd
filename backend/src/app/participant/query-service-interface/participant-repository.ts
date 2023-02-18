9a0be2cafa5d13033b668053288c1636e70a6e4c
shun57 <tuktuk.032@gmail.com>
2023-02-19 00:39:10 +0900

    fix: @ApiPropertyの補完を効かせるためにDTOを切り出す

--- backend/src/app/participant/query-service-interface/participant-repository.ts
+++ backend/src/app/participant/query-service-interface/participant-repository.ts
@@ -1,14 +1,4 @@
-export class ParticipantDTO {
-  public readonly id: string
-  public readonly name: string
-  public readonly enrollementStatus: string
-  public constructor(props: { id: string; name: string; enrollementStatus: string }) {
-    const { id, name, enrollementStatus } = props
-    this.id = id
-    this.name = name
-    this.enrollementStatus = enrollementStatus
-  }
-}
+import { ParticipantDTO } from "src/app/participant/dto/participant.dto"
 
 export interface ParticipantRepository {
   getAll(): Promise<ParticipantDTO[]>
