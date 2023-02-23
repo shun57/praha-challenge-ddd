df8795c9a661f6ff494b6456e99422523ca49f95
shun57 <tuktuk.032@gmail.com>
2023-02-23 21:33:55 +0900

    fix: インデントを2に統一

--- backend/src/domain/value-object/participant/participant-email.ts
+++ backend/src/domain/value-object/participant/participant-email.ts
@@ -1,20 +1,20 @@
 import { ValueObject } from 'src/shared/domain/value-object'
 
 export interface ParticipantEmailProps {
-    value: string;
+  value: string;
 }
 
 export class ParticipantEmail extends ValueObject<ParticipantEmailProps> {
 
-    get value(): string {
-        return this.props.value;
-    }
+  get value(): string {
+    return this.props.value;
+  }
 
-    private constructor(props: ParticipantEmailProps) {
-        super(props);
-    }
+  private constructor(props: ParticipantEmailProps) {
+    super(props);
+  }
 
-    public static create(props: ParticipantEmailProps): ParticipantEmail {
-        return new ParticipantEmail(props);
-    }
+  public static create(props: ParticipantEmailProps): ParticipantEmail {
+    return new ParticipantEmail(props);
+  }
 }
--- backend/src/domain/value-object/participant/participant-enrollment-status.ts
+++ backend/src/domain/value-object/participant/participant-enrollment-status.ts
@@ -1,32 +1,32 @@
 import { ValueObject } from 'src/shared/domain/value-object'
 
 export interface ParticipantEnrollmentStatusProps {
-    value: string;
+  value: string;
 }
 
 export const EnrollmentStatusType = {
-    enrolled: '在籍中',
-    recess: '休会中',
-    withdrawn: '退会済み',
+  enrolled: '在籍中',
+  recess: '休会中',
+  withdrawn: '退会済み',
 } as const;
 
 export type EnrollmentStatusType = typeof EnrollmentStatusType[keyof typeof EnrollmentStatusType]
 
 export class ParticipantEnrollmentStatus extends ValueObject<ParticipantEnrollmentStatusProps> {
 
-    get value(): string {
-        return this.props.value;
-    }
+  get value(): string {
+    return this.props.value;
+  }
 
-    private constructor(props: ParticipantEnrollmentStatusProps) {
-        super(props);
-    }
+  private constructor(props: ParticipantEnrollmentStatusProps) {
+    super(props);
+  }
 
-    public static create(props: ParticipantEnrollmentStatusProps): ParticipantEnrollmentStatus {
-        const statusList = Object.values(EnrollmentStatusType);
-        if (!statusList.find((status: EnrollmentStatusType) => status === props.value)) {
-            throw new Error('在籍ステータスが誤っています。');
-        }
-        return new ParticipantEnrollmentStatus(props);
+  public static create(props: ParticipantEnrollmentStatusProps): ParticipantEnrollmentStatus {
+    const statusList = Object.values(EnrollmentStatusType);
+    if (!statusList.find((status: EnrollmentStatusType) => status === props.value)) {
+      throw new Error('在籍ステータスが誤っています。');
     }
+    return new ParticipantEnrollmentStatus(props);
+  }
 }
