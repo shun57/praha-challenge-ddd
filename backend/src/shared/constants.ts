1c3361abe9d6e5fe9316d4c3878b06181b092067
shun57 <tuktuk.032@gmail.com>
2023-04-01 19:49:21 +0900

    fix: 依存解決用の定数定義の修正

--- backend/src/shared/constants.ts
+++ backend/src/shared/constants.ts
@@ -1,5 +1,9 @@
 export enum ConstantTokens {
   REPOSITORY = 'repository',
+  PARTICIPANT_REPOSITORY = 'participant-repository',
+  PAIR_REPOSITORY = 'pair-repository',
+  TEAM_REPOSITORY = 'team-repository',
+  MEMBERSHIP_REPOSITORY = 'membership-repository',
   REPOSITORY_QS = 'repository_qs',
   REPOSITORY_QS_PC = 'repository_qs_pc'
 }
