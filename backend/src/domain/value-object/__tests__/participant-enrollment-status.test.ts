3e8d625e40bb11a510732fe85a591697718cdde8
shun57 <tuktuk.032@gmail.com>
2023-04-15 00:24:34 +0900

    feat: 値オブジェクトの単体テスト作成

--- backend/src/domain/value-object/__tests__/participant-enrollment-status.test.ts
+++ backend/src/domain/value-object/__tests__/participant-enrollment-status.test.ts
@@ -7,19 +7,31 @@ import {
 describe('在籍ステータス', (): void => {
     test('在籍ステータスの値オブジェクトを生成できる', () => {
         const enrolled: ParticipantEnrollmentStatusProps = { value: EnrollmentStatusType.enrolled }
-        // 上記で生成したのを引数に値オブジェクトを生成
         const actual = ParticipantEnrollmentStatus.create(enrolled);
 
-        // 生成されている
         expect(actual).toBeInstanceOf(ParticipantEnrollmentStatus);
         expect(actual.value).toBe(enrolled.value)
     });
 
     test('想定していない在籍ステータスで値オブジェクトは生成できない', () => {
         const exist: ParticipantEnrollmentStatusProps = { value: "exist" }
-        // 値オブジェクトを生成するとエラーが起きる
         expect(() => {
             ParticipantEnrollmentStatus.create(exist);
         }).toThrowError('在籍ステータスが誤っています。');
     });
+
+    test('在籍中の場合、isEnrolled()はtrue', () => {
+        const enrolled: ParticipantEnrollmentStatusProps = { value: EnrollmentStatusType.enrolled }
+        const actual = ParticipantEnrollmentStatus.create(enrolled);
+
+        expect(actual.isEnrolled()).toBe(true)
+    });
+
+    test('在籍中ではに場合、isEnrolled()はfalse', () => {
+        const recess: ParticipantEnrollmentStatusProps = { value: EnrollmentStatusType.recess }
+        const actual = ParticipantEnrollmentStatus.create(recess);
+
+        expect(actual.isEnrolled()).toBe(false)
+    });
+
 });
