0b1629b1c0e6b77d8b979b3040397f2deffb61fb
shun57 <tuktuk.032@gmail.com>
2023-05-20 00:57:07 +0900

    fix: 使っていないpair-member削除

--- backend/src/domain/interface/pair-member/pair-member-repository.ts
+++
@@ -1,7 +0,0 @@
-import { PairMember } from "src/domain/entity/pair-member/pair-member";
-import { PairId } from "src/domain/value-object/pair/pair-id";
-
-
-export interface IPairMemberRepository {
-  getByPairIds(pairIds: PairId[]): Promise<PairMember[]>
-}
